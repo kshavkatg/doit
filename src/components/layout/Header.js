afdf4d79957f631890b1a1910eefe884a9173971
Shavkat Kasymov <kshavkatg@gmail.com>
2021-05-10 12:32:08 +0600

    start adding layout components, install react-icons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { FaPizzaSlice } from 'react-icons/fa'
 
 export const Header = () => {
+  
   return (
-    <div>
-      Header
-    </div>
+    <header className="header" data-testid="header" >
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todolist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
