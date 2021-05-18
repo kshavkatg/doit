e0f5bbc4e386d8b66ce9786be3a520ae90f38467
Shavkat Kasymov <kshavkatg@gmail.com>
2021-05-18 11:25:11 +0600

    add project provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Content } from "./components/layout/Content"
 import { Header } from "./components/layout/Header"
+import { ProjectsProvider } from './context'
 
 export const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
   );
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,7 @@
+import {
+  ProjectContext,
+  ProjectsProvider,
+  useProjectsValue
+} from './project-context'
+
+export { ProjectContext, ProjectsProvider, useProjectsValue }
