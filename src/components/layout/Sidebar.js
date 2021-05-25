17b8a75ac3d76ad998fa8db15131686b7839153e
Shavkat Kasymov <kshavkatg@gmail.com>
2021-05-25 11:16:37 +0600

    fixing sidebar sass

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -19,36 +19,42 @@ export const Sidebar = () => {
       <ul className="sidebar_generic">
         <li
           data-testid="inbox"
-          className={ active === 'inbox'? 'avtive' : undefined }
+          className={ active === 'inbox'? 'active' : undefined }
           onClick={() => {
             setActive('inbox')
             setSelectedProject('INBOX')
           }}
         >
-          <span><FaInbox /></span>
-          <span> Inbox</span>
+          <div>
+            <span><FaInbox /></span>
+            <span> Inbox</span>
+          </div>
         </li> 
         <li
           data-testid="today"
-          className={ active === 'today'? 'avtive' : undefined }
+          className={ active === 'today'? 'active' : undefined }
           onClick={() => {
             setActive('today')
             setSelectedProject('TODAY')
           }}
         >
-          <span><FaRegCalendar /></span>
-          <span> Today</span>
+          <div>
+            <span><FaRegCalendar /></span>
+            <span> Today</span>
+          </div>
         </li>
         <li
           data-testid="next_7"
-          className={ active === 'next_7'? 'avtive' : undefined }
+          className={ active === 'next_7'? 'active' : undefined }
           onClick={() => {
             setActive('next_7')
             setSelectedProject('NEXT_7')
           }}
         >
-          <span><FaRegCalendarAlt /></span>
-          <span> Next 7 days</span>
+          <div>
+            <span><FaRegCalendarAlt /></span>
+            <span> Next 7 days</span>
+          </div>
         </li>
       </ul>
       <div className="sidebar_middle" onClick={() => setShowProjects(!showProjects)}>
